fb15ce6f7fe53732c7ece70ec5ecea3ab637725f
Dominik Zogg <dominik.zogg@hotmail.com>
2024-06-16 10:29:33 +0200

    output stdout and stderr if something went wrong

--- build.js
+++ build.js
@@ -21,52 +21,58 @@ const ecmaScriptModuleDistDir = distDir + '/esm';
 
 rmSync(distDir, { recursive: true, force: true });
 
-execSync('./node_modules/.bin/tsc -b ./tsconfig.cjs.json ./tsconfig.esm.json ./tsconfig.types.json');
+try {
+  execSync('./node_modules/.bin/tsc -b ./tsconfig.cjs.json ./tsconfig.esm.json ./tsconfig.types.json');
 
-getAllFiles(commonJsDistDir).map((file) => {
-  const name = basename(file);
-  const fromFolder = dirname(file);
-  const toFolder = distDir + fromFolder.substring(commonJsDistDir.length);
+  getAllFiles(commonJsDistDir).map((file) => {
+    const name = basename(file);
+    const fromFolder = dirname(file);
+    const toFolder = distDir + fromFolder.substring(commonJsDistDir.length);
 
-  if (!name.match(/\.js$/)) {
-    return;
-  }
+    if (!name.match(/\.js$/)) {
+      return;
+    }
 
-  if (!existsSync(toFolder)) {
-    mkdirSync(toFolder, { recursive: true });
-  }
+    if (!existsSync(toFolder)) {
+      mkdirSync(toFolder, { recursive: true });
+    }
 
-  const fromPath = fromFolder + '/' + name;
-  const toPath = toFolder + '/' + name.replace(/\.js$/, '.cjs');
+    const fromPath = fromFolder + '/' + name;
+    const toPath = toFolder + '/' + name.replace(/\.js$/, '.cjs');
 
-  writeFileSync(
-    toPath,
-    readFileSync(fromPath, { encoding: 'utf8', flag: 'r' }).replace(/require\("\.([^"]+)"\)/g, 'require(".$1.cjs")'),
-  );
-});
+    writeFileSync(
+      toPath,
+      readFileSync(fromPath, { encoding: 'utf8', flag: 'r' }).replace(/require\("\.([^"]+)"\)/g, 'require(".$1.cjs")'),
+    );
+  });
 
-rmSync(commonJsDistDir, { recursive: true, force: true });
+  rmSync(commonJsDistDir, { recursive: true, force: true });
 
-getAllFiles(ecmaScriptModuleDistDir).map((file) => {
-  const name = basename(file);
-  const fromFolder = dirname(file);
-  const toFolder = distDir + fromFolder.substring(ecmaScriptModuleDistDir.length);
+  getAllFiles(ecmaScriptModuleDistDir).map((file) => {
+    const name = basename(file);
+    const fromFolder = dirname(file);
+    const toFolder = distDir + fromFolder.substring(ecmaScriptModuleDistDir.length);
 
-  if (!name.match(/\.js$/)) {
-    return;
-  }
+    if (!name.match(/\.js$/)) {
+      return;
+    }
 
-  if (!existsSync(toFolder)) {
-    mkdirSync(toFolder, { recursive: true });
-  }
+    if (!existsSync(toFolder)) {
+      mkdirSync(toFolder, { recursive: true });
+    }
 
-  const fromPath = fromFolder + '/' + name;
-  const toPath = toFolder + '/' + name.replace(/\.js$/, '.mjs');
+    const fromPath = fromFolder + '/' + name;
+    const toPath = toFolder + '/' + name.replace(/\.js$/, '.mjs');
 
-  writeFileSync(
-    toPath,
-    readFileSync(fromPath, { encoding: 'utf8', flag: 'r' }).replace(/from '\.([^']+)'/g, "from '.$1.mjs'"),
-  );
-});
+    writeFileSync(
+      toPath,
+      readFileSync(fromPath, { encoding: 'utf8', flag: 'r' }).replace(/from '\.([^']+)'/g, "from '.$1.mjs'"),
+    );
+  });
 
-rmSync(ecmaScriptModuleDistDir, { recursive: true, force: true });
+  rmSync(ecmaScriptModuleDistDir, { recursive: true, force: true });
+} catch (e) {
+  console.log(e.stdout.toString());
+  console.log(e.stderr.toString());
+}
+
